69cd4fceef35b86d9fd8c0704202ee12d1e9db05
masum raza <masumrazait@gmail.com>
2025-03-10 22:48:56 +0530

    code added

--- src/test/java/features/login.feature
+++ src/test/java/features/login.feature
@@ -1,5 +1,6 @@
 Feature: Login test
 
+  @SmokeTest
   Scenario Outline: User Login page
     Given User is on login page
     When User enters "<username>" and "<password>"
@@ -11,7 +12,7 @@ Feature: Login test
       | raza     | pass2    |
       | delhi    | pass3    |
 
-
+@RegressionTest
   Scenario: User Page Default Sign up
     Given User is on Practice landing page
       | Masum          |
--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -3,6 +3,6 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/features", glue = {"stepDefinition"}, plugin = {"pretty", "html:target/cucumber-reports"})
+@CucumberOptions(features = "src/test/java/features", glue = {"stepDefinition"}, tags = "@RegressionTest", plugin = {"pretty", "html:target/cucumber-reports"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
